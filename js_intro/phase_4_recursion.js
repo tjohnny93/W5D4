0d87cb96bd2cf54fed7b3f6b2c522fda626735ec
tjohnny93 <tjohnny93@berkeley.edu>
2019-06-13 17:15:43 -0700

    Complete Phase 4 #2

--- js_intro/phase_4_recursion.js
+++ js_intro/phase_4_recursion.js
@@ -1,13 +1,29 @@
 
-const range = function range(start, end) {
+function range(start, end) {
   if (start === end) {
     return [start];
-  } else {
+  } 
   let last = range(start, end-1);
   last.push(end);
-  return last
-  // return range(start, end-1).push(end);
-  
-}};
+  return last;
+  // return range(start, end-1).push(end); this does not workd .push returns lenght
+  // concat works.
+}
 
-console.log(range(1, 5));
+// console.log(range(1, 5));
+
+function sumRec(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  if (arr.length === 1) {
+    return arr[0];
+  }
+  
+  let last = sumRec(arr.slice(1));
+  last = last + arr[0];
+  return last;
+}
+
+let arr = [1,2,3,4,6];
+console.log(sumRec(arr));
